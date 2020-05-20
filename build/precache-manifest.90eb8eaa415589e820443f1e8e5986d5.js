64988c63bb7e7271efc0de137d26725c285da844
peterleist <peterle606@gmail.com>
2020-05-20 14:13:58 +0200

    Publish

---
+++ build/precache-manifest.90eb8eaa415589e820443f1e8e5986d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35ac1b63944ac02abf768bbdd422b0e3",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c343b53e9b00441b6f9e",
+    "url": "./static/css/main.215934a2.chunk.css"
+  },
+  {
+    "revision": "5ed28e986c9d3ccfaaa3",
+    "url": "./static/js/2.0f4066fb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.0f4066fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c343b53e9b00441b6f9e",
+    "url": "./static/js/main.edb645a1.chunk.js"
+  },
+  {
+    "revision": "3b5176513c78f0fdd135",
+    "url": "./static/js/runtime-main.03defe92.js"
+  }
+]);
