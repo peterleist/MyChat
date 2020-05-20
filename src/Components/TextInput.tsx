b8e1fe011bc5250e4e63c61f2e222b791f158f64
peterleist <peterle606@gmail.com>
2020-05-20 19:49:28 +0200

    Fix cant refersh issue

--- src/Components/TextInput.tsx
+++ src/Components/TextInput.tsx
@@ -45,4 +45,12 @@ export class TextInput extends Component<TextInputOptions>{
             </div>);
     }
 
+    //Fix: Cant refresh displayName field
+
+    componentDidUpdate(lastProp){
+        if(lastProp.value !== this.props.value){
+            this.setState({value: this.props.value})
+        }
+    }
+
 }
